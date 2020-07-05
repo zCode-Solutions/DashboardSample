04690ecd8185e91c2cae95e9bb1c564bd5039428
Zach <zfarley94@gmail.com>
2020-07-05 12:52:43 -0400

    amplify cognito ui added

---
+++ src/app.js
@@ -0,0 +1,35 @@
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/animate.min.css";
+import "./assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0";
+import "./assets/css/demo.css";
+import "./assets/css/pe-icon-7-stroke.css";
+import Amplify from 'aws-amplify';
+import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import awsconfig from './aws-exports';
+import AdminLayout from "layouts/Admin.jsx";
+Amplify.configure(awsconfig);
+
+function MainApp() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/admin" render={props => <AdminLayout {...props} />} />
+                <Redirect from="/" to="/admin/dashboard" />
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default function App() {
+    return (
+        <AmplifyAuthenticator>
+            <div>
+                <MainApp />
+                <AmplifySignOut />
+            </div>
+        </AmplifyAuthenticator>
+    )
+}
+
--- src/index.js
+++ src/index.js
@@ -25,15 +25,10 @@ import "./assets/css/animate.min.css";
 import "./assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0";
 import "./assets/css/demo.css";
 import "./assets/css/pe-icon-7-stroke.css";
-
+import App from './app'
 import AdminLayout from "layouts/Admin.jsx";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/admin" render={props => <AdminLayout {...props} />} />
-      <Redirect from="/" to="/admin/dashboard" />
-    </Switch>
-  </BrowserRouter>,
+  <App />,
   document.getElementById("root")
 );
